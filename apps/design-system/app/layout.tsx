5e70da4140b999012d5636f95190e3bd104e2d92
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-01-04 08:52:43 +0100

    chore: use local fonts :zap:

--- apps/design-system/app/layout.tsx
+++ apps/design-system/app/layout.tsx
@@ -1,8 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google"
+
 
 import "@workspace/ui/globals.css"
 import { Providers } from "@/components/providers"
 import { Metadata } from "next"
+import localFont from 'next/font/local'
+import { cn } from "@workspace/ui/lib/utils"
 
 
 export const metadata: Metadata = {
@@ -11,14 +13,40 @@ export const metadata: Metadata = {
 }
 
 
-const fontSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
-
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
+const mtnBrighterSans = localFont({
+  variable: '--mtn-brighter-sans',
+  src: [
+    {
+      path: './fonts/MTNBrighterSans-ExtraLight.ttf',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: './fonts/MTNBrighterSans-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './fonts/MTNBrighterSans-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/MTNBrighterSans-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/MTNBrighterSans-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonts/MTNBrighterSans-ExtraBold.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
 })
 
 export default function RootLayout({
@@ -27,13 +55,13 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={cn(mtnBrighterSans.variable, 'antialiased')}>
       <head>
         {/* Link to the favicon */}
         <link rel="icon" href="/img/mtn-logo-yellow.svg" />
       </head>
       <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
+        className={cn(mtnBrighterSans.variable, 'antialiased')}
       >
         <Providers>{children}</Providers>
       </body>
