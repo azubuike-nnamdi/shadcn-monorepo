c96b2d783bcd08f4e61e9dccbddd40e6f339081e
Your Other Name <your-email-account2@example.com>
2025-01-04 11:08:16 +0100

    chore: storybook re-setup

---
+++ apps/design-system/assets/fonts/index.ts
@@ -0,0 +1,4 @@
+export { default as MTNBrighterSansRegularTtf } from './mtn-sans/MTNBrighterSans-Regular.ttf';
+export { default as MTNBrighterSansRegularOtf } from './mtn-sans/MTNBrighterSans-Regular.otf';
+export { default as MTNBrighterSansRegularWoff } from './mtn-sans/MTNBrighterSans-Regular.woff';
+export { default as MTNBrighterSansRegularWoff2 } from './mtn-sans/MTNBrighterSans-Regular.woff2';
